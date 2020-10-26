2bb37e5bfc43a5cb661719b5a84be5d22048105c
Pedro Omar da S. Carvalho Neto <pedro.omar@upplay.com.br>
2020-10-26 07:22:32 -0300

    Adicionado estilos quando os inputs estão focused e filled. Iniciado captação de erros dos formulários

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,18 +1,23 @@
 import React, { useCallback, useRef } from 'react'
 import {
+	Alert,
 	Image,
 	KeyboardAvoidingView,
 	Platform,
 	ScrollView,
+	TextInput,
 	View,
 } from 'react-native'
 import Icon from 'react-native-vector-icons/Feather'
 import { useNavigation } from '@react-navigation/native'
 import { Form } from '@unform/mobile'
 import { FormHandles } from '@unform/core'
+import * as Yup from 'yup'
 
 import Button from '../../components/Button'
 import Input from '../../components/Input'
+
+import getValidationErrors from '../../utils/getValidationErrors'
 
 import {
 	Container,
@@ -25,17 +30,57 @@ import {
 
 import logoImg from '../../assets/logo.png'
 
-interface SignInProps {
+interface SignInFormData {
 	email: string
 	password: string
 }
 
 const SignIn: React.FC = () => {
 	const formRef = useRef<FormHandles>(null)
+	const inputPasswordRef = useRef<TextInput>(null)
 	const navigation = useNavigation()
 
-	const handleSignIn = useCallback((data: SignInProps) => {
-		console.log('data: ', data)
+	const handleSubmit = useCallback(async (data: SignInFormData): Promise<
+		void
+	> => {
+		try {
+			formRef.current?.setErrors({})
+
+			const schema = Yup.object().shape({
+				email: Yup.string()
+					.required('E-mail obrigatório')
+					.email('Digite um e-mail válido'),
+				password: Yup.string().required('Senha obrigatória'),
+			})
+
+			await schema.validate(data, {
+				abortEarly: false,
+			})
+
+			// const { email, password } = data
+
+			// await signIn({ email, password })
+
+			// history.push('/dashboard')
+		} catch (err) {
+			if (err instanceof Yup.ValidationError) {
+				const errors = getValidationErrors(err)
+
+				formRef.current?.setErrors(errors)
+			}
+
+			Alert.alert(
+				'Erro na autenticação',
+				'Ocorreu um erro ao fazer login, cheque as credenciais',
+			)
+
+			// addToast({
+			// 	type: 'error',
+			// 	title: 'Erro na autenticação',
+			// 	description:
+			// 		'Ocorreu um erro ao fazer login, cheque as credenciais',
+			// })
+		}
 	}, [])
 
 	return (
@@ -56,16 +101,29 @@ const SignIn: React.FC = () => {
 							<Title>Faça seu logon</Title>
 						</View>
 
-						<Form ref={formRef} onSubmit={handleSignIn}>
+						<Form ref={formRef} onSubmit={handleSubmit}>
 							<Input
+								autoCorrect={false}
+								autoCapitalize="none"
+								keyboardType="email-address"
 								name="email"
 								icon="mail"
 								placeholder="E-mail"
+								returnKeyType="next"
+								onSubmitEditing={() =>
+									inputPasswordRef.current?.focus()
+								}
 							/>
 							<Input
+								ref={inputPasswordRef}
+								secureTextEntry
+								returnKeyType="send"
 								name="password"
 								icon="lock"
 								placeholder="Senha"
+								onSubmitEditing={() => {
+									formRef.current?.submitForm()
+								}}
 							/>
 						</Form>
 						<Button onPress={() => formRef.current?.submitForm()}>
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -4,6 +4,7 @@ import {
 	KeyboardAvoidingView,
 	Platform,
 	ScrollView,
+	TextInput,
 	View,
 } from 'react-native'
 import Icon from 'react-native-vector-icons/Feather'
@@ -30,6 +31,8 @@ interface SignUpProps {
 }
 
 const SignUp: React.FC = () => {
+	const inputEmailRef = useRef<TextInput>(null)
+	const inputPasswordRef = useRef<TextInput>(null)
 	const formRef = useRef<FormHandles>(null)
 
 	const navigation = useNavigation()
@@ -57,16 +60,40 @@ const SignUp: React.FC = () => {
 						</View>
 
 						<Form ref={formRef} onSubmit={handleSignUp}>
-							<Input name="name" icon="user" placeholder="Nome" />
 							<Input
+								autoCapitalize="words"
+								name="name"
+								icon="user"
+								placeholder="Nome"
+								returnKeyType="next"
+								onSubmitEditing={() =>
+									inputEmailRef.current?.focus()
+								}
+							/>
+							<Input
+								ref={inputEmailRef}
+								keyboardType="email-address"
+								autoCorrect={false}
+								autoCapitalize="none"
 								name="email"
 								icon="mail"
 								placeholder="E-mail"
+								returnKeyType="next"
+								onSubmitEditing={() =>
+									inputPasswordRef.current?.focus()
+								}
 							/>
 							<Input
+								ref={inputPasswordRef}
+								secureTextEntry
 								name="password"
 								icon="lock"
 								placeholder="Senha"
+								textContentType="newPassword"
+								returnKeyType="send"
+								onSubmitEditing={() =>
+									formRef.current?.submitForm()
+								}
 							/>
 						</Form>
 
